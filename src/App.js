772d44d1154235ec1c87729671ae1f573ed1fbf3
abdelwahab youssef <abdelwahab.hamada@gmail.com>
2022-10-10 12:17:27 +0200

    brand news

--- src/App.js
+++ src/App.js
@@ -10,13 +10,13 @@ function App() {
       <div className='flex-1 flex justify-center overflow-hidden'>
       <div className=' absolute md:left-5 left-0 top-3.5 rounded backdrop-blur-xl z-50'>
             <img
-            alt="logo" className="w-10 md:w-16" src="https://res.cloudinary.com/abdelwahab-youssef/image/upload/v1662662719/favicon.ico_lrbzfr.png"/>
+            alt="logo" className="w-10 md:w-16" src={process.env.PUBLIC_URL+"/logo192.png"}/>
       </div>
       <Header/>
         <div className='relative overflow-overlay flex gap-12  py-16 scrollbar justify-center p-3 w-full rounded '>
           
           <Routes>
-              <Route path="portfolio-app/" element={<Outlet />}>
+              <Route path="portfolio/" element={<Outlet />}>
                 <Route path="" element={<Landing />} />
               </Route>
           </Routes>
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -4,8 +4,8 @@ const Hero = () => {
   return (
     <div className='flex flex-col gap-2'>
         <h2 className=' font-bold text-l'>Hi👋, i`m Abdelwahab</h2>
-        <h1 className=' text-5xl'>Software Developer</h1>
-        <p className='font-mono text-xl'>professional,creative,co-operative developer</p>
+        <h1 className=' text-5xl'>Back-end Developer</h1>
+        <p className='font-mono text-xl'>professional,creative,co-operative software developer</p>
     </div>
   )
 }
